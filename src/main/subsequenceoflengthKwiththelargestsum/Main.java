a4f8624539f1477d1f997ce69bd1794cf737f646
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2025-06-28 20:31:06 +0400

    Subsequence of Length K With the Largest Sum leetcode algorithm question solution added

---
+++ src/main/subsequenceoflengthKwiththelargestsum/Main.java
@@ -0,0 +1,48 @@
+package main.subsequenceoflengthKwiththelargestsum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        MySolution mySolution = new MySolution();
+        int[] result = mySolution.maxSubsequence(new int[]{-16, -13, 8, 16, 35, -17, 30, -8, 34, -2, -29, -35, 15, 13, -30, -34, 6, 15, 28, -23, 34, 28, -24, 15, -17, 10, 31, 32, -3, -36, 19, 31, -5, -21, -33, -18, -23, -37, -15, 12, -28, -40, 1, 38, 38, -38, 33, -35, -28, -40, 4, -15, -29, -33, -18, -9, -29, 20, 1, 36, -8, 23, -34, 16, -7, 13, 39, 38, 7, -7, -10, 30, 9, 26, 27, -37, -18, -25, 14, -36, 23, 28, -15, 35, -9, 1}, 8);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+class MySolution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+
+        if (k >= nums.length) {
+            return nums.clone();
+        }
+
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> subElementByCount = new HashMap<>();
+        for (int i = nums.length - k; i < nums.length; i++) {
+            int val = sorted[i];
+            subElementByCount.put(val, subElementByCount.getOrDefault(val, 0) + 1);
+        }
+
+        int[] result = new int[k];
+        int resultIndex = 0;
+        for (int num : nums) {
+            if (subElementByCount.getOrDefault(num, 0) > 0) {
+                result[resultIndex++] = num;
+                subElementByCount.put(num, subElementByCount.get(num) - 1);
+                if (resultIndex == k) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
